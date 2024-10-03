812514f2f080ffd18e0865a220c217b37d027742
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2024-10-03 23:53:39 +0530

    created userroute and controler

--- api/index.js
+++ api/index.js
@@ -2,13 +2,19 @@ import express from "express";
 import connectDB from "./src/db/index.js";
 const app = express();
 import "dotenv/config";
+import userRoute from "./routes/user.route.js";
 
 connectDB()
   .then(
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT||4000 , () => {
       console.log(`app listing at port ${process.env.PORT}`);
     })
   )
   .catch((err) => {
     console.log(err);
   });
+
+
+  app.use("/api/user",userRoute);
+  //here the final url will be localhost:3000/api/user/test
+  
